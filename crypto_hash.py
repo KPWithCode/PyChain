9b06842649877b53323c6c4eb268f876dd94fa26
Kyle Phillips <khillips375@yahoo.com>
2020-02-22 03:26:57 -0500

    Introduced lambda function

--- crypto_hash.py
+++ crypto_hash.py
@@ -1,17 +1,25 @@
 import hashlib
 import json
 
-def crypto_hash(data):
-    """
-    Return a sha-256 hash of the given data.
-    """
-    # Dumps data into string representation 
-    stringified_data = json.dumps(data)
 
-    return hashlib.sha256(stringified_data.encode('utf-8')).hexdigest()
+
+def crypto_hash(*args):
+    """
+    Return a sha-256 hash of the given arguments.
+    """
+     # Dumps data into string representation 
+    stringified_args = sorted(map(lambda data: json.dumps(data), args))
+    print(f'stringified_args: {stringified_args}')
+
+    joined_data = ''.join(stringified_args)
+    
+    print(f'joined_data: {joined_data}')
+
+    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 def main():
-    print(f"crypto_hash([2]): {crypto_hash([2])}")
+    print(f"crypto_hash('one',2,[3]): {crypto_hash('one',2,[3])}")
+    print(f"crypto_hash(2,'one',[3]): {crypto_hash(2,'one',[3])}")
 
 if __name__ == '__main__':
     main()
